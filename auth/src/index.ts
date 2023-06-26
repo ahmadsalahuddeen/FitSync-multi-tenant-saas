61f45e7e183233d5df6466d33cc69f34567c1b7c
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-06-27 03:14:46 +0530

    ingress setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,9 @@ import {json} from 'express'
 
 const app = express()
 app.use(json())
+app.get('/api/tenantId/currentTenant', (req, res)=>{
+  res.send('hii ther')
+})
 
 app.listen(3000, ()=>{
   console.log('listening port 3000')
