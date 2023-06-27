77d3f52880df0043337b7d052a413003c668b1a1
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-06-27 15:07:33 +0530

    validation to the req body

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,18 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 const route = express.Router()
+import { body } from 'express-validator';
 
-route.post('/api/users/signup', (req, res)=>{
-res.send('hi there')
+route.post('/api/users/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Provide a valid Email address'),
+
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters')
+], (req: Request, res: Response) => {
+  res.send('hi there')
 })
 
-export {route as signupRouter}; 
+export { route as signupRouter }; 
