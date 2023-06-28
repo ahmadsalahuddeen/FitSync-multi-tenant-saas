17fedbeb7ab17925eae486b18b9704a8ac78e435
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-06-29 03:58:36 +0530

    dtabase error handling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,3 +1,6 @@
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 import { Request, Response, NextFunction } from "express"
 
 export const errorHandler = (
@@ -6,7 +9,17 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('something went wrong', err)
+
+ if(err instanceof RequestValidationError){
+  console.log('handlilng this error as requeset validation error')
+ }
+
+
+ if( err instanceof DatabaseConnectionError){
+  console.log('handling database error')
+ }
+
+
   res.status(400).send({
     message:
       err.message
