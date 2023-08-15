d4bf8010fdefc61493a720ffdf0fbdefce1f5d11
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-08-15 07:20:58 +0530

    custom abstract error to handle bug in new error subclasses

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,16 @@
+
+
+export abstract class CustomError extends Error{
+
+ abstract statusCode: number;
+
+  constructor( ){
+    super()
+
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  abstract serializeErrors(): { message: string, field?: string;}[]
+
+
+}
