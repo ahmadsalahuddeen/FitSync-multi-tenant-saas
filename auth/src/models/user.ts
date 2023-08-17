2b0a248a41d74b47b45a1e857add59545ddb3dc0
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-08-17 08:03:37 +0530

    interface for user model, doc and new user creation

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,24 @@
-import mongoose  from "mongoose"; 
+import mongoose, { mongo }  from "mongoose"; 
+
+//interface that describes the properties to create a new User
+interface userAttrs  {
+email: string,
+password: string 
+}
+
+
+// interface that describes the properties
+// a User Model has
+interface userModel extends mongoose.Model<userDoc>{
+build(attrs: userAttrs): userDoc;
+}
+
+// interface that describes the properties
+// of a User Document
+interface userDoc extends mongoose.Document{
+  email: string,
+  password: string
+}
 
 
 const userSchema = new mongoose.Schema({
@@ -14,8 +34,18 @@ password: {
 
 })
 
+userSchema.statics.build = (attrs: userAttrs)=>{
+return new User(attrs)
+}
 
-const User  = mongoose.model('User', userSchema)
+
+const User  = mongoose.model<userDoc, userModel>('User', userSchema)
+
+
+
+const salah = User.build({email : 'asdfl', password : 'asdlfk'})
+
+
 
 
 export { User }
