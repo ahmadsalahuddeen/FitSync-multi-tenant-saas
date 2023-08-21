b5ce8d3dd38a56adfad47f196ba1d3035d10be5b
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-08-21 14:22:21 +0530

    mongoMemoryServer configured for tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,3 +1,29 @@
 import { MongoMemoryServer } from 'mongodb-memory-server',
 import mongoose from 'mongoose'
 import { app } from '../app'
+
+
+let mongo: any;
+
+// hook function that's going to run before all of the test
+beforeAll(async()=>{
+  mongo = new MongoMemoryServer()
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri)
+});
+
+// hook that run before each of the test
+beforeEach( async ()=> {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections){
+    await collection.deleteMany({})
+  }
+});
+
+// hook that run after all the tests are complete
+afterAll( async ()=>{
+  await mongo.stop();
+  await mongoose.connection.close();
+})
