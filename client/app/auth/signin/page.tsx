496c647dedd537668dde00bf27fe17ad4497eada
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-11-10 04:55:08 +0530

    major ui update

--- client/app/auth/signin/page.tsx
+++ client/app/auth/signin/page.tsx
@@ -159,11 +159,10 @@ const SignIn = (props: Props) => {
               </Link>
             </p>
           </Card>
-          {/* add form here */}
+
         </div>
       </div>
 
-      {/* <div className=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 "></div> */}
     </>
   );
 };
