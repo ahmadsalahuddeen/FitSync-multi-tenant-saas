140726c22ee16d605ac29492a165126a23888c14
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-11-11 03:12:00 +0530

    gym selector and other ui updates

--- client/app/(dashboard)/dashboard/layout.tsx
+++ client/app/(dashboard)/dashboard/layout.tsx
@@ -1,30 +1,53 @@
 import Header from "@/components/Header";
 import HeaderMobile from "@/components/HeaderMobile";
+import GymSwithcer from "@/components/gym-switcher";
+import { MainNav } from "@/components/main-nav";
+import { UserAccountNav } from "@/components/user-account-nav";
 import { siteConfig } from "@/config/site";
+import { getCurrentUser } from "@/lib/session";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: `Dashboard - ${siteConfig.name}`,
-  description: siteConfig.description
-}
+  description: siteConfig.description,
+};
 
-
-
-
-export default function RootLayout({
-children,
+export default async function RootLayout({
+  children,
 }: {
-  children: React.ReactNode
-}){
-  return (
-    <div>
-      <Header/>
-      <HeaderMobile/>
-      <main >  
+  children: React.ReactNode;
+}) {
+  const user = await getCurrentUser();
+  if (!user) {
+    return notFound();
+  }
 
-      {children}
-      </main>
+  return (
+    <div className="flex min-h-screen flex-col space-y-6">
+      <header className="sticky top-0 z-40 border-b bg-background">
+        <div className=" container flex h-16 items-center justify-between py-6 ">
+          <div className="flex ">
+            
+          <MainNav />
+          <div className="ml-6 hidden md:block">
+            <GymSwithcer />
+          </div>
+          </div>
+          <UserAccountNav
+            user={{
+              name: `${user.firstName} ${user.lastName}`,
+              image: null,
+              email: user.email,
+            }}
+          />
+        </div>
+      </header>
+      <Header />
+      <HeaderMobile />
+
+      <main>{children}</main>
     </div>
-  )
+  );
 }
---
+++ client/components/main-nav.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { useSelectedLayoutSegment } from "next/navigation"
+
+
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+
+
+interface MainNavProps {
+
+  children?: React.ReactNode
+}
+
+export function MainNav({  children }: MainNavProps) {
+  const segment = useSelectedLayoutSegment()
+  const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false)
+
+  return (
+    <div className="flex gap-6 md:gap-10">
+      <Link href="/" className="hidden items-center space-x-2 md:flex">
+        <Icons.logo className="text-green-600"/>
+        <span className="hidden font-bold sm:inline-block">
+          {siteConfig.name}
+        </span>
+      </Link>
+
+
+
+      <button
+        className="flex items-center space-x-2 md:hidden"
+        onClick={() => setShowMobileMenu(!showMobileMenu)}
+      >
+        {showMobileMenu ? <Icons.close /> : <Icons.logo />}
+        <span className="font-bold">Menu</span>
+      </button>
+     
+    </div>
+  )
+}
