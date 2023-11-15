b04f743c4af24b644d0a59e892ab92bb9d72febb
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-11-15 18:02:20 +0530

    base dashboard page update

--- client/app/(dashboard)/dashboard/page.tsx
+++ client/app/(dashboard)/dashboard/page.tsx
@@ -9,22 +9,36 @@ import { signOut, useSession } from "next-auth/react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getCurrentUser } from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
+import { EmptyPlaceholder } from "@/components/empty-placeholder";
 
 type Props = {};
+
 
 
 const Dashboard = async (props: Props) => {
    
 const user = await getCurrentUser()
 
-  
-  return <div >
+if(!user){
+    return  redirect('/auth/signin')
+}  
 
+const gym = user.gyms[0]
 
-    {!user ? 'login' : 'logout'}
+if(!gym) return(
+    <EmptyPlaceholder>
+    <EmptyPlaceholder.Icon name="post" />
+    <EmptyPlaceholder.Title>No gyms created</EmptyPlaceholder.Title>
+    <EmptyPlaceholder.Description>
+      You don&apos;t have any Gym yet. Add one right away.
+    </EmptyPlaceholder.Description>
+    TODO: add Gym Create button
+    {/* <GymCreateButton variant="outline" /> */}
+  </EmptyPlaceholder>
+)
 
-    
-    </div>;
+redirect(`${gym.gymId}`)
 };
 
 export default Dashboard;
