fbe7d96b6b50fb8d532d52d12170267b7b653ddb
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-11-24 14:45:39 +0530

    create gym form update

--- client/components/create-gym-form.tsx
+++ client/components/create-gym-form.tsx
@@ -41,44 +41,45 @@ import { Alert, AlertDescription } from "./ui/alert";
 import { CaretSortIcon, ExclamationTriangleIcon } from "@radix-ui/react-icons";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { getCountryCallingCode } from 'react-phone-number-input'
 
 import { Select } from "./ui/select";
 import { Input } from "./ui/input";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import {
   Command,
+  CommandDialog,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
+  CommandList,
 } from "./ui/command";
 import { Country, ICountry, IState, State } from "country-state-city";
 import { Check, CheckIcon, ChevronUpIcon, ChevronsUpDown } from "lucide-react";
+import { ScrollArea } from "./ui/scroll-area";
 
 type Props = {};
 
 const GymCreateForm = (props: Props) => {
-  
-  let countryData: ICountry[] = [] = Country.getAllCountries();
+  let countryData: ICountry[] = ([] = Country.getAllCountries());
   const [stateData, setStateData] = useState<IState[] | undefined>();
 
   const [country, setCountry] = useState({
     "name": "fakeittillmakeit",
-    "isoCode": "AF",
-   
+    "isoCode": "ID",
+    "phonecode": "62",
+    
 });
   const [state, setState] = useState<IState | undefined>();
 
   useEffect(() => {
-    setStateData(State.getStatesOfCountry(country?.isoCode ) ) ;
+    setStateData(State.getStatesOfCountry(country?.isoCode));
   }, [country]);
-
 
   useEffect(() => {
     stateData && setState(stateData[0]);
   }, [stateData]);
-
-
 
   console.log(countryData);
 
@@ -94,7 +95,7 @@ const GymCreateForm = (props: Props) => {
   } = useMutation({
     mutationFn: async (input: Input) => {
       try {
-        const response = await axios.post("/api/auth/tenant/signup", {
+        const response = await axios.post("/api/gym/create", {
           name: input.name,
           phoneNumber: input.phoneNumber,
         });
@@ -116,14 +117,17 @@ const GymCreateForm = (props: Props) => {
 
   async function onSubmit(input: Input) {
     try {
-      console.log(input, "iiiiiiiiiiiiiiiiiiiii");
+const phoneNumber = `${country.phonecode}${input.phoneNumber}`
+
+
+
+
     } catch (err) {
       console.log(err);
     }
   }
 
   const watcher = form.watch();
-  
 
   return (
     <Dialog>
@@ -159,7 +163,7 @@ const GymCreateForm = (props: Props) => {
               onSubmit={form.handleSubmit(onSubmit)}
               className=" relative space-y-3     "
             >
-              <div className={cn("space-y-9")}>
+              <div className={cn("space-y-6")}>
                 {/* gym Name */}
                 <FormField
                   control={form.control}
@@ -168,147 +172,170 @@ const GymCreateForm = (props: Props) => {
                     <FormItem>
                       <FormLabel>Gym name</FormLabel>
                       <FormControl>
-                        <Input
-                          {...field}
-                        />
+                        <Input {...field} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
                 <div className="grid gap-4 sm:grid-cols-2">
-                {/* country */}
+                  {/* country */}
 
-                <FormField
-          control={form.control}
-          name="country"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Language</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn(
-                        "w-[200px] justify-between",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value
-                        ? countryData.find(
-                            (el) => el.isoCode === field.value
-                          )?.name
-                        : "Select language"}
-                      <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0">
-                  <Command>
-                    <CommandInput placeholder="Search language..." />
-                    <CommandEmpty>No language found.</CommandEmpty>
-                    <CommandGroup>
-                      {countryData.map((el) => (
-                        <CommandItem
-                          value={el.name}
-                          key={el.name}
-                          onSelect={() => {
-                            setCountry(el)
-                            form.setValue("country", el.isoCode)
-                          }}
-                        >
-                          <CheckIcon
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              el.isoCode === field.value
-                                ? "opacity-100"
-                                : "opacity-0"
-                            )}
-                          />
-                          {el.name}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-             
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                  <FormField
+                    control={form.control}
+                    name="country"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Country</FormLabel>
+                        <Popover   modal={true}>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant="outline"
+                                role="combobox"
+                                className={cn(
+                                  "w-[200px] justify-between",
+                                  !field.value && "text-muted-foreground",
+                                )}
+                              >
+                                {field.value
+                                  ? countryData.find(
+                                      (el) => el.name === field.value,
+                                    )?.name
+                                  : "Select country"}
+                                <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-[200px] p-0 ">
+                            <Command>
+                              <CommandInput placeholder="Search country..." />
+                              <CommandList>
+                                <CommandEmpty>No language found.</CommandEmpty>
+                                <CommandGroup className=" ">
+                                  <ScrollArea
+                                    className="flex max-h-40 flex-col"
+                                    type="always"
+                                  >
+                                    {countryData.map((el) => (
+                                      <CommandItem
+                                        value={el.name}
+                                        key={el.name}
+                                        onSelect={() => {
+                                          setCountry(el);
+                                          form.setValue("country", el.name);
+                                        }}
+                                      >
+                                        <CheckIcon
+                                          className={cn(
+                                            "mr-2 h-4 w-4",
+                                            el.name === field.value
+                                              ? "opacity-100"
+                                              : "opacity-0",
+                                          )}
+                                        />
+                                        {el.name}
+                                      </CommandItem>
+                                    ))}
+                                  </ScrollArea>
+                                </CommandGroup>
+                              </CommandList>
+                            </Command>
+                          </PopoverContent>
+                        </Popover>
 
-                {/* state */}
-                <FormField
-           
-                  control={form.control}
-                  name="state"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-col">
-                      <FormLabel>state</FormLabel>
-                      <Popover>
-                        <PopoverTrigger
-                             disabled={country.name === 'fakeittillmakeit'}
-                        asChild>
-                          <FormControl>
-                            <Button
-                              variant="outline"
-                              role="combobox"
-                              className={cn(
-                                "w-[200px] justify-between",
-                                !field.value && "text-muted-foreground",
-                              )}
-                            >
-                              {field.value
-                                ? stateData?.find(
-                                    (el) =>
-                                      el.name === field.value,
-                                  )?.name
-                                : "Select state"}
-                              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-[200px] p-0">
-                          <Command>
-                            <CommandInput placeholder="Search country..." />
-                            <CommandEmpty>No state found.</CommandEmpty>
-                            <CommandGroup>
-                              {stateData?.map((el) => (
-                                <CommandItem
-                                  value={el.name}
-                                  key={el.name}
-                                  onSelect={() => {
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-                                    form.setValue("state", el.name);
-                                  }}
+                  {/* state */}
+                  <FormField
+                    control={form.control}
+                    name="state"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>state</FormLabel>
+                        <Popover modal={true}>
+                          <PopoverTrigger
+                            disabled={country.name === "fakeittillmakeit"}
+                            asChild
+                          >
+                            <FormControl>
+                              <Button
+                                variant="outline"
+                                role="combobox"
+                                className={cn(
+                                  "w-[200px] justify-between",
+                                  !field.value && "text-muted-foreground",
+                                )}
+                              >
+                                {field.value
+                                  ? stateData?.find(
+                                      (el) => el.name === field.value,
+                                    )?.name
+                                  : "Select state"}
+                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-[200px] p-0">
+                            <Command>
+                              <CommandInput placeholder="Search state..." />
+                              <CommandEmpty>No state found.</CommandEmpty>
+                              <CommandGroup>
+                                <ScrollArea
+                                  className="flex h-40 max-h-[200px] flex-col"
+                                  type="always"
                                 >
-                                  <Icons.check
-                                    className={cn(
-                                      "mr-2 h-4 w-4",
-                                      el.name === field.value
-                                        ? "opacity-100"
-                                        : "opacity-0",
-                                    )}
-                                  />
-                                  {el.name}
-                                </CommandItem>
-                              ))}
-                            </CommandGroup>
-                          </Command>
-                        </PopoverContent>
-                      </Popover>
+                                  {stateData?.map((el) => (
+                                    <CommandItem
+                                      value={el.name}
+                                      key={el.name}
+                                      onSelect={() => {
+                                        form.setValue("state", el.name);
+                                      }}
+                                    >
+                                      <Icons.check
+                                        className={cn(
+                                          "mr-2 h-4 w-4",
+                                          el.name === field.value
+                                            ? "opacity-100"
+                                            : "opacity-0",
+                                        )}
+                                      />
+                                      {el.name}
+                                    </CommandItem>
+                                  ))}
+                                </ScrollArea>
+                              </CommandGroup>
+                            </Command>
+                          </PopoverContent>
+                        </Popover>
 
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                {/* phone Number */}
+                <FormField
+                  control={form.control}
+                  name="phoneNumber"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone Number</FormLabel>
+                      <FormControl>
+                        <Input type="number" placeholder="" {...field} />
+                      </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
-</div>
                 <div className="flex gap-4">
                   <Button className="flex-1 	" type="submit">
-                    submit
+                    Create Gym
                     {/* {isLoading ? "Creating Gym..." : "Create Gym "}
                     {isLoading ? (
                       <Icons.spinner className="ml-2 h-4 w-4 animate-spin" />
