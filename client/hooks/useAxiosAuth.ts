be5335ba75a9e1ae6c060493ec4377d42dc489cb
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-11-29 23:46:12 +0530

    add interceptor in in use axios hook

--- client/hooks/useAxiosAuth.ts
+++ client/hooks/useAxiosAuth.ts
@@ -1,5 +1,6 @@
 "use client";
 import  { axiosAuth } from "@/lib/axios";
+import { useGymStore } from "@/store/gym";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
@@ -7,12 +8,18 @@ import { useEffect } from "react";
 
 // hook to manage headers in axios request with accestoken 
 const useAxiosAuth =  () => {
+
+  const {gym } = useGymStore()
+
   const { data: session } =  useSession();
   useEffect(() => {
     const requestIntercept = axiosAuth.interceptors.request.use(
       (config) => {
         if (!config.headers["Authorization"]) {
           config.headers["Authorization"] = `Bearer ${session?.backendTokens?.accessToken}`;
+        }
+        if(gym.id !== ''){
+          config.params.gymId = gym.id
         }
         return config;
       },
